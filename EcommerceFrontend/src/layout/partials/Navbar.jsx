81db496089df6f1e0d09d05e93fc3412d210cf28
Rohandesk <rohandesk372@gmail.com>
2025-04-22 18:24:09 +0530

    abc

--- EcommerceFrontend/src/layout/partials/Navbar.jsx
+++ EcommerceFrontend/src/layout/partials/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
         })}
       </div>
 
-      <div className="absolute top-0 h-full w-full justify-between items-center flex text-white px-10 text-3xl">
+      <div className="absolute top-0 h-full w-full justify-between items-center flex text-white px-10 text-3xl abc">
         <button onClick={previousSlide} className='group p-[10px] rounded-full bg-white cursor-pointer hover:shadow-[inset_200px_0_0_0_#ff5252]'>
         <svg width="20px" height="20px" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg" className='text-black group-hover:text-white'>
         <path d="M19 12H5M5 12L12 19M5 12L12 5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
