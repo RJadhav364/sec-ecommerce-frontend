9071dea8e72e9e9bbe3110736908824da54a3ad2
RJadhav364 <rj8556141@gmail.com>
2025-05-24 21:22:30 +0530

    single product page is completed need to apply css

--- EcommerceFrontend/src/pages/ParticularProduct/SingleProduct.jsx
+++ EcommerceFrontend/src/pages/ParticularProduct/SingleProduct.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import { getParticularProduct } from './services/SingleProductRelatedApi';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
+import ProductRating from "../../components/ProductRating"
 
 const SingleProduct = () => {
   const {id} = useParams();
@@ -57,7 +58,8 @@ const SingleProduct = () => {
               style={{
                 '--swiper-navigation-color': '#fff',
                 '--swiper-pagination-color': '#fff',
-                width: "512px"
+                width: "512px",
+                height: "500px"
               }}
               loop={true}
               spaceBetween={10}
@@ -71,8 +73,8 @@ const SingleProduct = () => {
                 data && data.images && data.images.map((image,index) => (
                     // console.log(image)
                     <SwiperSlide key={index}>
-                      <div className='h-full rounded-md'>
-                        <img src={image} className='' />
+                      <div className='h-full rounded-md w-full'>
+                        <img src={image} className='w-full' />
                       </div>
                     </SwiperSlide>
                   ))
@@ -85,6 +87,72 @@ const SingleProduct = () => {
         </div>
         <div className='w-full lg:w-[60%] pr-2 pl-2 lg:pr-10 lg:pl-10 dark:text-white'>
           <h1 class="text-[18px] sm:text-[22px] font-[600] mb-2">Apple iPhone 15 (Blue, 128 GB)</h1>
+          <div className='flex'>
+            <span className='productBrand'>productBrand</span><span className='productRating flex flex-row'><ProductRating stars={4} /></span>
+          </div>
+          <div className='pt-[16px]'>
+            <div>
+              <div>
+                productOldPrice
+              </div>
+              <div>
+                productCurrentPrice
+              </div>
+            </div>
+            <div>
+              Available IN Stock
+            </div>
+          </div>
+          <div className='mt-3 pr-10 mb-5'>
+            <p>Product Description</p>
+          </div>
+          <div className='flex items-center gap-4 py-4'>
+            <div className='qtyBoxWrapper w-[70px]'>
+              <input type="number" name="" id="" />
+            </div>
+            <button
+            className="MuiButtonBase-root MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeSmall MuiButton-textSizeSmall MuiButton-colorPrimary MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeSmall MuiButton-textSizeSmall MuiButton-colorPrimary btn-org addToCartBtn btn-border flex w-full btn-sm gap-2 css-uiq2rh dark:text-text-color border border-[#ff5252] hover:text-white hover:shadow-[inset_300px_0_0_0_#ff5252] justify-center items-center py-[5px] transition duration-[.4s] ease-in-out cursor-pointer rounded-[5px]"
+            tabIndex="0"
+            type="button"
+            >
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 24 24"
+              className="text-[18px]"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path fill="none" d="M0 0h24v24H0V0z"></path>
+              <path d="M15.55 13c.75 0 1.41-.41 1.75-1.03l3.58-6.49A.996.996 0 0 0 20.01 4H5.21l-.94-2H1v2h2l3.6 7.59-1.35 2.44C4.52 15.37 5.48 17 7 17h12v-2H7l1.1-2h7.45zM6.16 6h12.15l-2.76 5H8.53L6.16 6zM7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zm10 0c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z"></path>
+              </svg>{" "}
+              Add to Cart
+            </button>
+          </div>
+          <div className="addToWishList">
+            <button
+            className="MuiButtonBase-root MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeSmall MuiButton-textSizeSmall MuiButton-colorPrimary MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeSmall MuiButton-textSizeSmall MuiButton-colorPrimary btn-org addToCartBtn btn-border flex w-full btn-sm gap-2 css-uiq2rh dark:text-text-color border border-[#ff5252] hover:text-white hover:shadow-[inset_300px_0_0_0_#ff5252] justify-center items-center py-[5px] transition duration-[.4s] ease-in-out cursor-pointer rounded-[5px]"
+            tabIndex="0"
+            type="button"
+            >
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 24 24"
+              className="text-[18px]"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path fill="none" d="M0 0h24v24H0V0z"></path>
+              <path d="M15.55 13c.75 0 1.41-.41 1.75-1.03l3.58-6.49A.996.996 0 0 0 20.01 4H5.21l-.94-2H1v2h2l3.6 7.59-1.35 2.44C4.52 15.37 5.48 17 7 17h12v-2H7l1.1-2h7.45zM6.16 6h12.15l-2.76 5H8.53L6.16 6zM7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zm10 0c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z"></path>
+              </svg>{" "}
+              Add to Cart
+            </button>
+          </div>
         </div>
       </div>
     </section>
