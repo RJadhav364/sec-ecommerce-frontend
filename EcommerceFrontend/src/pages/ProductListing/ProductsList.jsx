2fd5e360bad510477933244e6150ad1c5a6f61ee
Rohandesk <rohandesk372@gmail.com>
2025-06-04 18:28:58 +0530

    after click on wishlist api called and product data send

--- EcommerceFrontend/src/pages/ProductListing/ProductsList.jsx
+++ EcommerceFrontend/src/pages/ProductListing/ProductsList.jsx
@@ -44,7 +44,7 @@ const ProductsList = () => {
                     {img: "https://serviceapi.spicezgold.com/download/1742462729828_zoom_0-1673275594.webp",
                         percent: 30},
     ];
-    const {token} = useCustomerStore();
+    const {token, id: userId} = useCustomerStore();
     const [changedInId, setChangedInId] = useState("")
     const paramMap = {
         catId: 'categoryId',
@@ -81,13 +81,14 @@ const ProductsList = () => {
     useEffect(()=>{
         getData();
     },[filterValue])
-    const addProductInFavourite = async() => {
+    const addProductInFavourite = async(userId,id,productDiscount, productName, productOldPrice,productCurrentPrice, productRating,productInStock, productBrand, categoryName, categoryId) => {
         const response = await fetch(`http://localhost:9000/favourite/add-wishlist`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${token}`
             },
+            body: JSON.stringify({userId,id,productDiscount, productName, productOldPrice,productCurrentPrice, productRating,productInStock, productBrand, categoryName, categoryId})
         });
     }
   return (
@@ -177,7 +178,7 @@ const ProductsList = () => {
                                         <path d="m15 3 2.3 2.3-2.89 2.87 1.42 1.42L18.7 6.7 21 9V3h-6zM3 9l2.3-2.3 2.87 2.89 1.42-1.42L6.7 5.3 9 3H3v6zm6 12-2.3-2.3 2.89-2.87-1.42-1.42L5.3 17.3 3 15v6h6zm12-6-2.3 2.3-2.87-2.89-1.42 1.42 2.89 2.87L15 21h6v-6z"></path>
                                         </svg>
                                     </button>
-                                    <button className="!w-[35px] !h-[35px] !min-w-[35px] !rounded-full !bg-white text-black hover:!bg-primary  group css-iyey26 cursor-pointer flex justify-center items-center" tabIndex="0" type="button" onClick={() => {addProductInFavourite()}}>
+                                    <button className="!w-[35px] !h-[35px] !min-w-[35px] !rounded-full !bg-white text-black hover:!bg-primary  group css-iyey26 cursor-pointer flex justify-center items-center" tabIndex="0" type="button" onClick={() => {addProductInFavourite(userId,id,productDiscount, productName, productOldPrice,productCurrentPrice, productRating,productInStock, productBrand, categoryName, categoryId)}}>
                                         <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" className="text-[18px] !text-black" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M458.4 64.3C400.6 15.7 311.3 23 256 79.3 200.7 23 111.4 15.6 53.6 64.3-21.6 127.6-10.6 230.8 43 285.5l175.4 178.7c10 10.2 23.4 15.9 37.6 15.9 14.3 0 27.6-5.6 37.6-15.8L469 285.6c53.5-54.7 64.7-157.9-10.6-221.3zm-23.6 187.5L259.4 430.5c-2.4 2.4-4.4 2.4-6.8 0L77.2 251.8c-36.5-37.2-43.9-107.6 7.3-150.7 38.9-32.7 98.9-27.8 136.5 10.5l35 35.7 35-35.7c37.8-38.5 97.8-43.2 136.5-10.6 51.1 43.1 43.5 113.9 7.3 150.8z"></path>
                                         </svg>
