3c6c7298831b15f883b7a950c448188f3302eed6
Rohandesk <rohandesk372@gmail.com>
2025-06-25 18:15:50 +0530

    on myprofile when user click on change password then password input fields will get on page plus input field logic has changed, from navbar unwanted hover removed need to resolved nested a issue we have separate Link , and in header just a question mark has added so that error will not throw on frontend

--- EcommerceFrontend/src/components/My_profile.jsx
+++ EcommerceFrontend/src/components/My_profile.jsx
@@ -1,49 +1,81 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import useCustomerStore from '../store/customerStore';
 
 const My_profile = () => {
-    const {username , email , token , id} = useCustomerStore();
-    const [customerUpdatedData , setCustomerUpdatedData] = useState({
+    const {username , email , token , id, phonenumber} = useCustomerStore();
+     const [isOpen, setIsOpen] = useState(false);
+    const [customerUpdatedData , setCustomerUpdatedData] = useState(
+    {
         username: username,
         email: email,
         phonenumber: "",
     });
     const handleUpdateCustomerData = (e) => {
         e.preventDefault();
+        // for(let key in extraData.current){
+        //     setCustomerUpdatedData({[key] : extraData.current[key].value})
+        // }
         console.log(customerUpdatedData);
     }
+    const opnPasswordModel = () => {
+        setIsOpen(!isOpen);
+        // handleInputChange("username", "")
+        // for (let key in extraData.current) {
+        //     if (extraData.current[key]) {
+        //         extraData.current[key].value = ''; // reset value to empty
+        //     }
+        // }
+    }
+    useEffect(() => {
+        setCustomerUpdatedData(
+            isOpen
+            ? { oldPassword: "", newpassword: "", confirmPassword: "" }
+            : { username: username, email: email, phonenumber: "" }
+        );
+        // console.log(customerUpdatedData)
+    }, [isOpen]);
   return (
     <>
         <div className="py-5 px-3 border-b border-[rgba(0,0,0,0.1)] dark:border-[#4c4c4c] flex justify-between">
-            <h2>My Profile</h2>
-            <p className="mt-0 mb-0 text-[#ff2727] p[2px_5px] cursor-pointer hover:underline">Change Password</p>
+            <h2>{isOpen ? "Change Password" : "My Profile"}</h2>
+            <button className="mt-0 mb-0 text-[#ff2727] p[2px_5px] cursor-pointer hover:underline" onClick={opnPasswordModel}>Change Password</button>
         </div>
         <form className='mt-8 px-3 py-3 pb-8'>
             <div className="grid grid-cols-2 gap-10">
-                <div className="col">
-                    <div className=""><div className="relative w-full  h-10 z-[1]">
-                        <input className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 disabled:cursor-not-allowed transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2   placeholder:opacity-0 focus:placeholder:opacity-100 text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900 dark:focus:border-white" placeholder=" " defaultValue={customerUpdatedData.username} onChange={(e) => {
-                        setCustomerUpdatedData((prev) => ({
-                            ...prev,
-                            username: e.target.value,
-                        }));
-                    }} /><label className={`peer-focus:text-text-ff5252 transition-all peer-focus:-translate-y-[16px] peer-focus:text-[12px] dark:peer-focus:bg-[#000000] peer-focus:bg-white peer-focus:px-[5px]  ${customerUpdatedData.username != "" ? "dark:bg-[#000000] bg-white px-[5px] -translate-y-[16px] text-text-ff5252 text-[12px] absolute top-[8px] left-[12px] z-[1]" : "absolute top-[8px] left-[12px] z-[-1] peer-focus:z-[1]"}`}>Username </label>
-                    </div></div>
-                </div>
-                <div className="col">
-                    <div className=""><div className="relative w-full h-10 z-[1]"><input className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50  disabled:cursor-not-allowed transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2   placeholder:opacity-0 focus:placeholder:opacity-100 text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900 dark:focus:border-white" onChange={(e) => {
-                       setCustomerUpdatedData((prev) => ({
-                            ...prev,
-                            email: e.target.value,
-                        }));}}  placeholder=" " defaultValue={customerUpdatedData.email} disabled /><label className={`peer-focus:text-text-ff5252 transition-all peer-focus:-translate-y-[16px] peer-focus:text-[12px] dark:peer-focus:bg-[#000000] peer-focus:bg-white peer-focus:px-[5px]  ${customerUpdatedData.email != "" ? "dark:bg-[#000000] bg-white px-[5px] -translate-y-[16px] text-text-ff5252 text-[12px] absolute top-[8px] left-[12px] z-[1]" : "absolute top-[8px] left-[12px] z-[-1] peer-focus:z-[1]"}`}>Email </label></div></div>
-                </div>
-                <div className="col">
-                    <div className=""><div className="relative w-full h-10 z-[1]"><input className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 disabled:cursor-not-allowed transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2   placeholder:opacity-0 focus:placeholder:opacity-100 text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900 dark:focus:border-white" placeholder=" " defaultValue={customerUpdatedData.phonenumber} onChange={(e) => {
-                        setCustomerUpdatedData((prev) => ({
-                            ...prev,
-                            phonenumber: e.target.value,
-                        }));}} /><label className={`peer-focus:text-text-ff5252 transition-all peer-focus:-translate-y-[16px] peer-focus:text-[12px] dark:peer-focus:bg-[#000000] peer-focus:bg-white peer-focus:px-[5px]  ${customerUpdatedData.phonenumber != "" ? "dark:bg-[#000000] bg-white px-[5px] -translate-y-[16px] text-text-ff5252 text-[12px] absolute top-[8px] left-[12px] z-[1]" : "absolute top-[8px] left-[12px] z-[-1] peer-focus:z-[1]"}`}>Phone Number </label></div></div>
-                </div>
+                {
+                    // isOpen ? (
+                    //     customerUpdatedData && Object.keys(customerUpdatedData).map((value) => (
+                    //         // console.log("render"),
+                    //         <div className="col">
+                    //             <div className="">
+                    //                 <div className="relative w-full  h-10 z-[1]">
+                    //                     <input className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 disabled:cursor-not-allowed transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2   placeholder:opacity-0 focus:placeholder:opacity-100 text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900 dark:focus:border-white" placeholder=" " defaultValue={customerUpdatedData[value]} onChange={(e) => { setCustomerUpdatedData((prev) => ({...prev,[value]: e.target.value,
+                    //                     }));}} />
+                    //                     <label className={`peer-focus:text-text-ff5252 transition-all peer-focus:-translate-y-[16px] peer-focus:text-[12px] dark:peer-focus:bg-[#000000] peer-focus:bg-white peer-focus:px-[5px]  ${customerUpdatedData[value] != "" ? "dark:bg-[#000000] bg-white px-[5px] -translate-y-[16px] text-text-ff5252 text-[12px] absolute top-[8px] left-[12px] z-[1]" : "absolute top-[8px] left-[12px] z-[-1] peer-focus:z-[1]"}`}>{value} </label>
+                    //                 </div>
+                    //             </div>
+                    //         </div>
+                    //     ))
+                    // ) : (
+                        customerUpdatedData && Object.keys(customerUpdatedData).map((value, index) => (
+                            // console.log(customerUpdatedData),
+                            <div className="col" key={index}>
+                                <div className="">
+                                    <div className="relative w-full  h-10 z-[1]">
+                                        <input className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 disabled:cursor-not-allowed transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2   placeholder:opacity-0 focus:placeholder:opacity-100 text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900 dark:focus:border-white" placeholder=" " value={customerUpdatedData[value]} 
+                                        onChange={(e) => {
+                                            setCustomerUpdatedData((prev) => ({...prev,[value]: e.target.value,}));
+                                            // handleInputChange(value, e.target.value)
+                                        }} 
+                                        // ref={(e)=>{extraData.current.username = e}}
+                                        />
+                                        <label className={`peer-focus:text-text-ff5252 transition-all peer-focus:-translate-y-[16px] peer-focus:text-[12px] dark:peer-focus:bg-[#000000] peer-focus:bg-white peer-focus:px-[5px]  ${customerUpdatedData[value] != "" ? "dark:bg-[#000000] bg-white px-[5px] -translate-y-[16px] text-text-ff5252 text-[12px] absolute top-[8px] left-[12px] z-[1]" : "absolute top-[8px] left-[12px] z-[-1] peer-focus:z-[1]"}`}>{value} </label>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    // )
+                }
             </div>
             {/* </div> */}
             <p className='mt-[30px]'>
--- EcommerceFrontend/src/layout/partials/Navbar.jsx
+++ EcommerceFrontend/src/layout/partials/Navbar.jsx
@@ -27,22 +27,35 @@ const Navbar = ({blockBg}) => {
           <div className='mx-auto w-[1400px] py-[20px]'>
               {/* <div></div> */}
               <ul className='flex gap-3 items-center justify-center'>
-              <li className='group text-[16px]font-semibold relative transition duration-[.4s] ease-in-out shadow-[inset_0_0_0_0_#ff5252] text-[#ff5252] hover:text-white hover:shadow-[inset_200px_0_0_0_#ff5252] p-[10px] rounded-[8px]'><Link to={"/"}>Home
+              <li className='group text-[16px] font-semibold relative transition duration-[.4s] ease-in-out shadow-[inset_0_0_0_0_#ff5252] text-[#ff5252] hover:text-white hover:shadow-[inset_200px_0_0_0_#ff5252] p-[10px] rounded-[8px]'><Link to={"/"}>Home
               </Link></li>
                 {
                   options && options.passedData && options.passedData.length > 0 && options.passedData.map(({id,categoryName,subNavbar,children}) => (
-                    <li key={id} className='group text-[16px]font-semibold relative transition duration-[.4s] ease-in-out shadow-[inset_0_0_0_0_#ff5252] text-[#ff5252] hover:text-white hover:shadow-[inset_200px_0_0_0_#ff5252] p-[10px] rounded-[8px]'>
+                    // <React.Fragment key={id}>
+                    <li key={id} className='group text-[16px] font-semibold relative transition duration-[.4s] ease-in-out shadow-[inset_0_0_0_0_#ff5252] text-[#ff5252] hover:text-white hover:shadow-[inset_200px_0_0_0_#ff5252] p-[10px] rounded-[8px] cursor-pointer'>
                       <Link to={`products?catId=${id}`}>{categoryName}
                       </Link>
                       { subNavbar == true && (
-                        <ul className='absolute z-[999] bg-white left-[-15%] w-[122px] top-[125%] border-[1px] shadow-md  opacity-0 group-hover:opacity-100 group-hover:inline-block transform group-hover:transform group-hover:origin-top transition-all group-hover:transition-all group-hover:duration-[.3s] group-hover:ease-in-out max-h-0 group-hover:max-h-[200px]'>
+                        <ul className='absolute z-[999] bg-white left-[-3%] w-[122px] top-[103%] border-[1px] shadow-md  opacity-0  transform  transition-all   max-h-0 invisible pointer-events-none group-hover:inline-block group-hover:visible group-hover:pointer-events-auto group-hover:max-h-[200px] group-hover:opacity-100 group-hover:transform group-hover:origin-top group-hover:transition-all group-hover:duration-[.3s] group-hover:ease-in-out'>
+                          {/* group-hover:opacity-100 group-hover:inline-block
+                          group-hover:transform group-hover:origin-top
+                          group-hover:transition-all group-hover:duration-[.3s]
+                          group-hover:ease-in-out
+                          group-hover:max-h-[200px]
+                           */}
                           {
                             children && children.length > 0 && children.map(({subCategoryName,id,parentCategory, children}) => (
                               <React.Fragment key={id}>
                                 <Link to={`products?subCatId=${id}`}>
                                 <li key={id} className='py-[5px] px-[30px] text-black cursor-pointer relative group/subgroup'>{subCategoryName}
+                                  {/* group/subgroup */}
                                 
-                                <ul className='absolute left-[101%] w-[122px] top-[-2%] transform translate-x-0 opacity-0 origin-left bg-white group-hover/subgroup:opacity-100 transition-all duration-300 ease-in-out max-h-0 group-hover/subgroup:max-h-[200px] group-hover/subgroup:-translate-x-[5px]'>
+                                <ul className='absolute left-[106%] w-[122px] top-[-2%] transform translate-x-0 opacity-0 origin-left bg-white  transition-all duration-300 ease-in-out max-h-0 group-hover/subgroup:-translate-x-[5px]  group-hover/subgroup:max-h-[200px] group-hover/subgroup:opacity-100 invisible pointer-events-none group-hover/subgroup:visible group-hover/subgroup:pointer-events-auto
+                                '>
+                                  {/* group-hover/subgroup:-translate-x-[5px] 
+                                      group-hover/subgroup:max-h-[200px]
+                                      group-hover/subgroup:opacity-100
+                                  */}
                                   {
                                     // children && children.length > 0 && children.map(({children,_id}) => (
                                       children.length > 0 && children.map(({thirdLevelCatName,_id}) => (
@@ -76,6 +89,7 @@ const Navbar = ({blockBg}) => {
                       )
                       }
                     </li>
+                      // </React.Fragment>
                   ))
                 }
               </ul>
