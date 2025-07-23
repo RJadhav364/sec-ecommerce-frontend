87c6af0a353847cf938325f39d707f7745ba3b8b
Rohandesk <rohandesk372@gmail.com>
2025-07-23 18:01:48 +0530

    just try to make skelton loader dynamic

--- EcommerceFrontend/src/components/Abc.jsx
+++ EcommerceFrontend/src/components/Abc.jsx
@@ -1,12 +1,14 @@
 import ProductLoader from "./ProductLoader";
 import PopularProductSlider from "../views/home/PopularProductSlider";
+import { useState } from "react";
 
 const Abc = ({popularProductsData , isPopularProductLoading}) => {
+    // const [ProductSkeltonLoader , setProductSkeltonLoader] = useState("")
     let ProductSkeltonLoader = 5;
   return (
     <div className="pb-[30px] dark:bg-darkbg-highlight homeSlider">
             { isPopularProductLoading ? (
-                <ProductLoader skeltonNo={ProductSkeltonLoader} classes="grid grid-cols-5 gap-[20px]" />
+                <ProductLoader skeltonNo={ProductSkeltonLoader} classes={`grid 2xl:grid-cols-5 xl:grid-cols-5 lg:grid-cols-3 md:grid-cols-2 gap-[20px]`} />
             ) : (
                 <PopularProductSlider data={popularProductsData} />
             )
