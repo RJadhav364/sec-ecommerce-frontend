de7edf9b9718e9ba6fdc769a76097db6faba0376
Rohandesk <rohandesk372@gmail.com>
2025-12-08 15:02:30 +0530

    on customer login page theme wise some css changed and commong button element implemented on that page

--- EcommerceFrontend/src/pages/Login/Login.jsx
+++ EcommerceFrontend/src/pages/Login/Login.jsx
@@ -5,6 +5,7 @@ import { emailregx } from '../../Validation/inputValidation';
 import { loginCustomerDetail } from './services/loginApis';
 import { useNavigate } from 'react-router-dom';
 import useCustomerStore from '../../store/customerStore';
+import Button from '../../components/Button';
 
 const Login = () => {
     const navigate = useNavigate();
@@ -116,21 +117,21 @@ const Login = () => {
   return (
     <section className='dark:bg-darkbg-highlight dark:text-white font-display-Montserrat'>
         <div className='mx-auto w-[1400px] py-[50px] flex items-center flex-col'>
-            <div className='dark:bg-[#181818] bg-[#ebebeb] p-[20px] w-[400px] rounded-[10px] shadow-[0px_0px_7px_0px_#ebebeb]'>
+            <div className='dark:bg-[#181818] bg-[#ffffff] p-[20px] w-[400px] rounded-[10px] dark:shadow-[0px_0px_12px_0px_#000] shadow-[0px_0px_12px_0px_#ebebeb]'>
                 <h3 className="text-center text-[20px] font-[600]">Login to your account</h3>
-                <h5 className='text-center text-[18px] text-[#bdbdbd] mt-[5px]'>Don't have an account yet? <Link to={'/register'} className='hover:underline hover:underline-offset-1 hover:decoration-[#bdbdbd] font-bold'>Sign up</Link></h5>
+                <h5 className='text-center text-[18px] dark:text-[#bdbdbd] mt-[5px]'>Don't have an account yet? <Link to={'/register'} className='hover:underline hover:underline-offset-1 hover:decoration-[#bdbdbd] font-bold'>Sign up</Link></h5>
                 <div>
                     <div className="form-group w-full mt-5 mb-5 flex flex-col gap-[20px]">
                         <div className="flex flex-col gap-1.5">
                             <label htmlFor="email">Email</label>
-                            <input type="text" name='email' className='dark:bg-[#111111] bg-[#cccccc] outline-none p-[10px] dark:shadow-[0px_0px_6px_2px_#000] shadow-[0px_0px_6px_2px_#ccc] w-full rounded-md' ref={(e)=>{customerLoginDetails.current.email = e}} />
+                            <input type="text" name='email' className='dark:bg-[#111111] bg-[#fff] outline-none p-[10px] dark:shadow-[0px_0px_6px_2px_#000] shadow-[0px_0px_6px_2px_#ccc] w-full rounded-md' ref={(e)=>{customerLoginDetails.current.email = e}} />
                         </div>
                         <div className="flex flex-col gap-1.5">
                             <label htmlFor="password">Password</label>
-                            <input type="password" name='password' className='dark:bg-[#111111] bg-[#cccccc] outline-none p-[10px] dark:shadow-[0px_0px_6px_2px_#000] shadow-[0px_0px_6px_2px_#ccc] w-full rounded-md' ref={(e)=>{customerLoginDetails.current.password = e}} />
+                            <input type="password" name='password' className='dark:bg-[#111111] bg-[#fff] outline-none p-[10px] dark:shadow-[0px_0px_6px_2px_#000] shadow-[0px_0px_6px_2px_#ccc] w-full rounded-md' ref={(e)=>{customerLoginDetails.current.password = e}} />
                         </div>
                     </div>
-                    <button className='w-full p-2.5 bg-[#ff5252] rounded-md inline-block mt-[10px]' onClick={loginCustomer}>Login</button>
+                    <Button classes='w-full p-2.5 dark:bg-[#ff5252] bg-[#f76d6d] text-white rounded-md inline-block mt-[10px] cursor-pointer font-semibold' btnLabel="Login" onClick={loginCustomer} />
                 </div>
             </div>
         </div>
