5677a71f753c99e4c5caecdb586c3950f05adfa1
Rohandesk <rohandesk372@gmail.com>
2026-03-11 11:59:25 +0530

    on getting wishlist api on confirm for logout modal not getting hide issue resolved

--- EcommerceFrontend/src/layout/partials/Header.jsx
+++ EcommerceFrontend/src/layout/partials/Header.jsx
@@ -269,7 +269,7 @@ const Header = ({ handleChild1Data }) => {
         onClick={toggleDropdown}
         className={`${isOpen ? "fixed top-0 left-0 right-0 bottom-0 z-[10] bg-[rgba(145,145,145,0.5)]" : "hidden"}`}
       ></div>
-      <Relogin isReloginModelOpen={isReloginModelOpen} setIsReloginModelOpen={setIsReloginModelOpen} />
+      <Relogin isReloginModelOpen={isReloginModelOpen} setIsReloginModelOpen={() => setIsReloginModelOpen(false)} />
     </>
   );
 };
